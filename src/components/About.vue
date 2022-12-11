b1df2e5fe82d535df2aa091aae3799de31b9687d
tateplayer <lilrobot0@gmail.com>
2022-12-11 00:43:26 -0700

    Actual new resume

--- src/components/About.vue
+++ src/components/About.vue
@@ -54,7 +54,7 @@
                 </ul>
             </div>
             <div class="about-links-container">
-                <a href="/static/about/tateplayer-resume.pdf"><button class="about-link-button">Resume Document</button></a>
+                <a href="/static/about/tate-player-resume-12112022.pdf"><button class="about-link-button">Resume Document</button></a>
             </div>
         </div>
     </div>
